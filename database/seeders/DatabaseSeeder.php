64f265791407f71fc9d5d1dce159590aceb4a017
Ben Wrigley <ben@jammin.co.uk>
2022-12-30 12:53:10 +0000

    Re initialisation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,15 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // \App\Models\User::factory(10)->create();
 
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $user = Post::factory(10)->create([
-            'user_id' => $user->id
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- resources/views/components/author-link.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['author'])
-
-<h5 class="font-bold"><a href="/?author={{$author->username}}">{{$author->name}}</a></h5>
