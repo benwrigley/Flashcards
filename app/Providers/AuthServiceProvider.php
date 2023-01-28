761aef6118e4c6f4df6c5976f972bc3c933e8d4e
Ben Wrigley <ben@jammin.co.uk>
2023-01-28 13:07:00 +0000

    New email registration

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage())
+                ->subject('Verify Email Address')
+                ->action('Verify Email Address', $url)
+                ->view('emails.verify', compact('url'));
+        });
 
         //
     }
---
+++ resources/views/emails/verify.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+
+    <div class="flex flex-col">
+        <div>
+            Thank you for registering with flashcards! Please click the link below to verify your email address and let's get you testing!
+        </div>
+
+        <div class="rounded-2xl bg-gray-800 hover:bg-gray-600">
+            <a href="{{ $url }}"> Verify Email </a>
+        </div>
+    </div>
+
+</x-layout>
