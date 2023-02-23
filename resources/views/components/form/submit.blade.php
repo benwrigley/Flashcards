03a5ded432b3fb78bf96ea4fc67499e25fe1d2ba
Ben Wrigley <ben@jammin.co.uk>
2023-02-23 19:45:08 +0000

    Change button to include type and click

--- resources/views/components/form/submit.blade.php
+++ resources/views/components/form/submit.blade.php
@@ -1,3 +1,9 @@
-@props(['label','bgcolor'])
+@props(['label','bgcolor','type','click'])
 
-<button type="submit" class="{{ $bgcolor ?? 'bg-gray-700' }} text-white rounded py-2 px-4 hover:animate-pulse hover:bg-blue-500 hover:scale-110 duration-500"> {{ $label }}</button>
+<button
+    type="{{$type ?? 'submit'}}"
+    x-on:click="{{$click ?? ''}}"
+    class="{{ $bgcolor ?? 'bg-gray-700' }} text-white rounded py-2 px-4 hover:animate-pulse hover:bg-blue-500 hover:scale-110 duration-500"
+>
+    {{ $label }}
+</button>
