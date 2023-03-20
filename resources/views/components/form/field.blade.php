cdb4d32098f31b9a3cc299f63a890de4fcc77fe4
Ben Wrigley <ben@jammin.co.uk>
2023-03-20 11:24:49 +0000

    Add error bag so that flashcard and topic forms can show errors

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -5,7 +5,8 @@
     'value',
     'size',
     'length',
-    'required'])
+    'required',
+    'errorbag'])
 
 {{-- <div class="mb-4 w-4/6"> --}}
     <input class="border border-gray-400 p-2 text-gray-700 rounded text-sm lg:text-xl w-full"
@@ -18,7 +19,7 @@
             value="{{ $value }}"
             {{ $required ?? '' }}
     >
-    @error($id)
+    @error($id,$errorbag ?? null)
         <p class="text-red-700 text-sm lg:text-xl lg:p-2 mb">{{ $message }}</p>
     @enderror
 {{-- </div> --}}
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,4 +1,4 @@
-@props(['id','type','placeholder','value','required','rows'])
+@props(['id','type','placeholder','value','required','rows','errorbag'])
 
 {{-- <div class="mb-4 w-4/6"> --}}
 
@@ -9,7 +9,7 @@
         rows="{{ $rows }}"
         placeholder="{{ $placeholder }}"
     >{{ $value  }}</textarea>
-    @error($id)
+    @error($id,$errorbag ?? null)
         <p class="text-red-700 p-2 mb">{{ $message }}</p>
     @enderror
 {{-- </div> --}}
