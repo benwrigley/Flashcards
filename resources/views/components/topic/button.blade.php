8a47df3dea05f3d0f1181987c4578a75c1dc940e
Ben Wrigley <ben@jammin.co.uk>
2023-03-20 11:25:31 +0000

    Have description as a tooltip on topics

--- resources/views/components/topic/button.blade.php
+++ resources/views/components/topic/button.blade.php
@@ -1,6 +1,12 @@
-@props(['href','label','click'])
+@props(['href','label','click','description'])
 
-<div x-on:click="{{$click}}" class="p-1 m-2 text-white hover:scale-125 duration-500 lg:text-xl text-sm">
+<div
+    x-on:click="{{$click}}"
+    class="p-1 m-2 text-white hover:scale-125 duration-500 lg:text-xl text-sm"
+    x-data="{description:false}"
+    x-on:mouseover="description = true"
+    x-on:mouseleave="description = false"
+>
 
     {{ $slot }}
 
@@ -11,4 +17,9 @@
     >
         {{$label}}
     </a>
+    @isset($description)
+        <div x-show="description" class="text-xs bg-gray-600 w-1/2 absolute left-1/2 rounded">
+            {{$description}}
+        </div>
+    @endisset
 </div>
--- resources/views/components/topic/grid.blade.php
+++ resources/views/components/topic/grid.blade.php
@@ -5,6 +5,7 @@
         @foreach ($topics as $topic)
             <x-topic.button
                 :label="$topic->name"
+                :description="$topic->description"
                 :href="'/topics/' . $topic->slug"
                 click=''
                 class="{{ isSet($topic->background) ? $topic->background : 'bg-gray-600' }} hover:bg-gray-400"
