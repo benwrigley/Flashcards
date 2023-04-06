d0bdbf68050a977e9e0a25dfcaeffa5f078d5a91
Ben Wrigley <ben@jammin.co.uk>
2023-04-06 08:41:47 +0100

    New Vue Pages and Components for Email verification and for the Register view

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+    import { provide } from 'vue'
+    import { Link,useForm } from '@inertiajs/vue3'
+
+    import Layout from '@/Shared/Layout.vue'
+    import FormLayout from '@/Shared/FormLayout.vue'
+    import FormInput from '@/Shared/FormInput.vue'
+    import FormButton from '@/Shared/FormButton.vue'
+    import WarningTriangle from '@/Shared/SVG/WarningTriangle.vue'
+
+
+    const form = useForm({
+        email: null
+    });
+
+    provide('form', form);
+
+
+</script>
+
+
+<template>
+    <Layout title="Verify">
+
+
+        <div class="w-full grid place-items-center">
+            <div class="mb-10">
+                <div class="flex justify-center items-center mb-10 bg-gray-800 rounded p-5">
+                    <WarningTriangle class="fill-red-600 mr-5" height="30" width="30" />
+                    <p class="text-3xl text-center">Your account is not yet verified.</p>
+
+                </div>
+                <div class="text-center">
+                    Please check your email for the verification link
+                </div>
+            </div>
+        <FormLayout method="post" :routeName="route('verification.resend')" class="lg:flex lg:justify-between lg:items-baseline text-gray-500 text-center" :box="false">
+            <div class="text-xl lg:text-2xl mb-2 lg:mb-0">
+                No Email? Enter your email to resend
+            </div>
+
+            <FormInput
+                id="email"
+                placeholder="Email"
+                class="lg:w-1/3"
+
+            />
+
+            <FormButton
+                label="Resend"
+            />
+
+        </FormLayout>
+    </div>
+    </Layout>
+</template>
---
+++ resources/js/Pages/Register/Create.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+    import { provide } from 'vue'
+    import { Link,useForm } from '@inertiajs/vue3'
+
+    import Layout from '@/Shared/Layout.vue'
+    import FormLayout from '@/Shared/FormLayout.vue'
+    import FormInput from '@/Shared/FormInput.vue'
+    import FormButton from '@/Shared/FormButton.vue'
+    import LinkButton from '@/Shared/LinkButton.vue'
+
+
+    const form = useForm({
+        name: null,
+        email: null,
+        password: null,
+    });
+
+    provide('form', form);
+
+
+</script>
+
+
+<template>
+    <Layout title="Register">
+        <div class="w-full grid place-items-center">
+        <FormLayout title="Register for Flashcards" method="post" :routeName="route('register.store')">
+                <FormInput
+                        id="name"
+                        placeholder="Name"
+
+                    />
+                <FormInput
+                    id="email"
+                    placeholder="Email"
+
+                />
+                <FormInput
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    />
+
+                <div class="flex justify-around w-full">
+                    <LinkButton
+                        label="Login"
+                        class="bg-gray-900 text-gray-500"
+                        :link="route('login')"
+                    />
+                    <FormButton
+                        label="Register"
+                    />
+                </div>
+        </FormLayout>
+    </div>
+    </Layout>
+</template>
