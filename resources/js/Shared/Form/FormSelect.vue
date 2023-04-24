05cd15d2f6a6c141ee1912c0db85182e8ad3357f
Ben Wrigley <ben@jammin.co.uk>
2023-04-24 19:20:25 +0100

    New Form Select file

---
+++ resources/js/Shared/Form/FormSelect.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+    import { inject, computed } from 'vue';
+    import FormError from '@/Shared/Form/FormError.vue';
+
+
+    const props = defineProps({
+
+        items: {
+            type: Array,    // expects name value pairs
+            required: true
+        },
+        label: {
+            type: String,
+            default: null
+        },
+        id : String,
+        modelValue : String,
+        error: String,
+    });
+
+    const form = inject('form');
+
+
+</script>
+
+<template>
+    <div class="flex mb-5 items-center w-max space-x-3 text-xl">
+        <p class="mr-2">{{ label }}</p>
+
+
+        <select
+            :id="id"
+            v-model="form[props.id]"
+            class="text-gray-900 rounded p-1.5"
+        >
+            <option v-for="(id, name) in items" :key="id" :value="id" :selected="id === topic.topic_id">
+                {{ name }}
+            </option>
+        </select>
+
+    </div>
+    <FormError v-if="form.errors[props.id]">
+            {{ form.errors[props.id] }}
+    </FormError>
+
+</template>
