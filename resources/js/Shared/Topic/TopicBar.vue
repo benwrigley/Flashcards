90b3f184ae7f44796249445610cabe78133d38d7
Ben Wrigley <ben@jammin.co.uk>
2023-04-27 18:08:43 +0100

    use drageneter and dragleave to change display of items that can and cannot be dropped on

--- resources/js/Shared/Topic/TopicBar.vue
+++ resources/js/Shared/Topic/TopicBar.vue
@@ -1,6 +1,7 @@
 <script setup>
 
-import { inject, ref, defineEmits, watch} from 'vue';
+import { inject, ref, defineEmits, watch, computed} from 'vue';
+import { usePage } from '@inertiajs/vue3';
 import ChevronDown from '@/Shared/SVG/ChevronDown.vue';
 import TopicBlock from './TopicBlock.vue';
 import TopicBarButtons from '@/Shared/Topic/TopicBarButtons.vue'
@@ -9,6 +10,14 @@ import { updateTopicParent } from '@/composables/updateTopicParent';
     const props = defineProps({
         topic: Object
     });
+
+    //keep track of whether we are currently dragging over a child so we know which one to open and close
+    let childCount = ref(0);
+
+    function childCounter(v){
+        childCount.value += v;
+        open.value = (childCount.value > 0);
+    }
 
     //managing which containers should be open
     const openTree = inject('openTree');
@@ -25,12 +34,7 @@ import { updateTopicParent } from '@/composables/updateTopicParent';
         open.value = !open.value;
     }
 
-    //keep track of whether we are currently dragging over a child so we know which one to open and close
-    let childCount = 0;
 
-    function childCounter(v){
-        childCount += v;
-    }
 
 
     //**DRAG AND DROP
@@ -43,12 +47,18 @@ import { updateTopicParent } from '@/composables/updateTopicParent';
     //when an element is dropped on this element check its not a child and then submit the update to backend
     function dropHandler(e) {
 
+        childCount.value=0;
+
         const draggedElement = document.getElementById(draggedItem.value);
         const thisElement = e.target;
 
-
         // prevent setting parent to self or child
         if (draggedElement.contains(thisElement)) {
+
+            return;
+        }
+
+        if (props.topic.flashcards_count > 0){
             return;
         }
 
@@ -60,16 +70,34 @@ import { updateTopicParent } from '@/composables/updateTopicParent';
         draggedItem.value = item;
     }
 
-    //increment the counter when dragging over/leaving a child
-    function dragInside(b){
-        if (b){
-            childCounter(1);
-        }
-        else{
-            childCounter(-1);
-        }
-        open.value = (childCount > 0);
+    function dragEnterHandler(e){
+        childCounter(1);
+
     }
+
+    function dragLeaveHandler(e){
+        childCounter(-1);
+
+    }
+
+
+    let additionalClass = computed(() => {
+        if (childCount.value > 0 && props.topic.flashcards_count > 0 && draggedItem.value !== props.topic.id){
+                return "filter grayscale opacity-40 ";
+        }
+        return "";
+    });
+
+    //increment the counter when dragging over/leaving a child
+    // function dragInside(b){
+    //     if (b){
+    //         childCounter(1);
+    //     }
+    //     else{
+    //         childCounter(-1);
+    //     }
+    //     open.value = (childCount > 0);
+    // }
 
 
 </script>
@@ -82,9 +110,11 @@ import { updateTopicParent } from '@/composables/updateTopicParent';
         @dragstart.stop="dragStartHandler($event,topic.id)"
         @drop.stop="dropHandler($event)"
         @dragover.prevent
-        @dragenter="$emit('childCounter',1);dragInside(true)"
-        @dragleave="$emit('childCounter',-1);dragInside(false)"
+        @dragenter="$emit('childCounter',1);dragEnterHandler($event)"
+        @dragleave="$emit('childCounter',-1);dragLeaveHandler($event)"
         @touchstart.stop="dragStartHandler($event,topic.id)"
+        :class="additionalClass"
+        class=""
     >
         <div class="rounded p-2 flex items-center relative">
 
