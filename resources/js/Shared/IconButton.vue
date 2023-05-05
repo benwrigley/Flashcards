1f7fe1329c19e284bb7b116b2c92133f6d5f5531
Ben Wrigley <ben@jammin.co.uk>
2023-05-05 17:26:29 +0100

    Allow icon button to have a disabled mode

--- resources/js/Shared/IconButton.vue
+++ resources/js/Shared/IconButton.vue
@@ -6,6 +6,10 @@
         tooltip : {
             type: String,
             default: null
+        },
+        disable : {
+            type: Boolean,
+            default: false
         }
     });
 
@@ -16,14 +20,17 @@
 <template>
     <div class="relative flex justify-center">
         <div
-            class="bg-gray-700 p-1 rounded hover:scale-125 duration-200 hover:bg-blue-500"
+            :class="{
+                'bg-gray-700 p-1 rounded hover:scale-125 duration-200 hover:bg-blue-500' : !disable,
+                'bg-gray-800 p-1 rounded' : disable
+            }"
             @mouseover="showtip = true"
             @mouseout="showtip = false"
         >
             <slot /> <!-- svg -->
         </div>
         <div
-            v-if="showtip && tooltip !== null"
+            v-if="showtip && tooltip !== null && !disable"
             class="absolute -top-12 -left-10 bg-blue-500 rounded p-2 text-sm w-max duration-200"
             @mouseout="showtip = false"
         >
