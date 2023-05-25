842215c5fcbbd259e6b7304ebfaab1c8182d289a
Ben Wrigley <ben@jammin.co.uk>
2023-05-25 18:59:17 +0100

    Adding these to safelist for dynamic building

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,13 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+	  'shadow-blue-300',
+    'shadow-purple-400',
+    'shadow-red-400',
+    'shadow-green-400',
+    'shadow-yellow-400',
+    'shadow-gray-600',
+    'shadow-pink-400'
+   ]
 }
